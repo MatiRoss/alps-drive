9332d5b4eb77ef1341a1fc53d68a334340f52a6a
MatiRoss <mati04.ross@gmail.com>
2021-04-26 14:20:27 +0200

    Refacto, gestion de l'imbrication des fichiers et rajout de commentaires

--- drive.js
+++ drive.js
@@ -1,27 +1,33 @@
+// Inclusion des modules var "exemple" = require('....')
 const fs = require('fs/promises')
 const os = require('os')
 const path = require('path')
 
+// Déclaration du path du dossier temporaire nommé 'root'
 const ALPS_DRIVE_ROOT = path.join(os.tmpdir(), 'root');
 
 function logFolderExist() {
     console.log('Le dossier ' + ALPS_DRIVE_ROOT + ' existe bien');
 }
 
+// Fonction qui crée le dossier temporaire 'root' dont le path est stocké dans la variable "ALPS_DRIVE_ROOT"
 function createRootFolderNoVerify() {
     return fs.mkdir(ALPS_DRIVE_ROOT);
 }
 
+// Fonction qui vérifie l'existence du dossier temporaire via la méthode access du module fs
 function rootFolderExists() {
     return fs.access(ALPS_DRIVE_ROOT);
 }
 
+// Fonction qui vérifie l'existence du dossier temporaire et le crée s'il n'existe pas déjà
 function createRootFolder() {
     return rootFolderExists()
         .then(logFolderExist)
         .catch(createRootFolderNoVerify);
 }
 
+// Fonction qui liste tous les dossiers et fichiers à la racine du drive et qui stocke dans un tableau le nom de chaque fichier/dossier ainsi que le champ isFolder (true or false)
 function listAllFoldersAndFiles(path = ALPS_DRIVE_ROOT) {
     const allFoldersAndFilesPromise = fs.readdir(path, {withFileTypes: true});
     return allFoldersAndFilesPromise.then((folderAndFilesList) => {
@@ -39,6 +45,7 @@ function listAllFoldersAndFiles(path = ALPS_DRIVE_ROOT) {
         })
 }
 
+// Fonction qui retourne le contenu du dossier ou du fichier (via téléchargement dans le cas d'un fichier)
 function getFolderOrFileByName(name) {
     const fileName = path.join(ALPS_DRIVE_ROOT, name)
     const content = fs.stat(fileName)
@@ -62,6 +69,7 @@ function getFolderOrFileByName(name) {
 
 }
 
+// Fonction qui crée un dossier. Si la destination n'inclut pas un dossier, il est créé à la racine, sinon, il est créé dans le dossier en question (voir côté server.js)
 function createFolder(name, folder = false) {
     function hasFolder(folder) {
         if (folder) {
@@ -82,7 +90,7 @@ function createFolder(name, folder = false) {
         })
 }
 
-
+// Fonction qui supprime un dossier. Si la destination n'inclut pas un dossier, ce sera le dossier à la racine qui sera supprimé, sinon il s'agit d'un sous dossier qui sera supprimé
 function deleteFolderOrFile(name, folder = false) {
     function hasFolder(folder) {
         if (folder) {
@@ -105,7 +113,7 @@ function deleteFolderOrFile(name, folder = false) {
 
 }
 
-
+// Fonction qui permet d'uploader un fichier à n'importe quel endroit sur le drive (même logique que la création et suppression de dossier)
 function uploadFile(file, name, folder = false) {
     function hasFolder(folder) {
         if (folder) {
@@ -127,7 +135,7 @@ function uploadFile(file, name, folder = false) {
         })
 }
 
-
+// Exports des différentes fonctions créées afin qu'elles puissent être réutilisées ailleurs dans le code
 module.exports = {
     createRootFolder: createRootFolder,
     listAllFoldersAndFiles: listAllFoldersAndFiles,
--- server.js
+++ server.js
@@ -1,24 +1,35 @@
+// Import du fichier drive.js
 const drive = require('./drive')
+// Import du module express qui permet la création du serveur
 const express = require('express')
+// Instanciation du serveur
 const app = express()
+// Déclaration du port sur lequel le serveur écoutera (ici, le port 3000. Exemple : localhost:3000)
 const port = 3000
 
+// Import du module busboy de Express qui va permettre l'upload de fichiers sur le drive
 const bb = require('express-busboy')
 
+// Configuration du module busboy
 bb.extend(app, {
     upload: true,
     path: 'C:/Users/mati0/AppData/Local/Temp'
 
 })
 
+// La fonction express.static permet de servir des fichiers statiques comme des images, fichiers CSS, HTML et des fichiers JavaScript.
+// Ici, elle permet de servir des fichiers stockés dans le dossier appelé 'frontend'
 app.use(express.static('frontend'));
 
+// Fonction qui lance le serveur. La méthode listen permet de relier la connexion entre l'host et le port spécifié, ici le port 3000.
 function start() {
     app.listen(port, () => {
     })
 }
 
+// Traitement lors d'une requête de type 'GET' à l'url '/api/drive'. Dans ce cas là, appel de la fonction qui liste les dossiers et fichiers à l'emplacement indiqué
 app.get('/api/drive', (req, res) => {
+
     drive.listAllFoldersAndFiles().then(result => {
         res.send(result)
     })
@@ -27,8 +38,10 @@ app.get('/api/drive', (req, res) => {
         })
 })
 
-app.get('/api/drive/:folder/:name?', (req, res) => {
-    drive.getFolderOrFileByName(`${req.params.folder}${req.params.name ? "/" + req.params.name : ""}`).then(result => {
+//Traitement lors d'une requête de type 'GET' à n'importe quel emplacement sur le drive
+app.get('/api/drive/*/:name?', (req, res) => {
+
+    drive.getFolderOrFileByName(`${req.params[0]}${req.params.name ? "/" + req.params.name : ""}`).then(result => {
         res.send(result)
     })
         .catch((err) => {
@@ -39,29 +52,11 @@ app.get('/api/drive/:folder/:name?', (req, res) => {
 
 })
 
-app.post('/api/drive', (req, res) => {
+//Traitement lors d'une requête de type 'POST' à l'emplacement où l'on se trouve et qui appelle la fonction qui crée un dossier
+app.post('/api/drive/*/:name?', (req, res) => {
+    const folder = req.params[0]
     const name = req.query.name
 
-    const regex = /^[a-zA-Z0-9]*$/
-    const test = regex.test(name)
-
-    if (test) {
-        drive.createFolder(name).then(result => {
-            res.send(result)
-        })
-            .catch((err) => {
-                console.log(err)
-            })
-
-    } else {
-        res.sendStatus(400)
-        console.log('Le nom du dossier ne doit contenir que des caractères alphanumériques')
-    }
-})
-
-app.post('/api/drive/:folder/:name?', (req, res) => {
-    const folder = req.params.folder
-    const name = req.query.name
 
     const regex = /^[a-zA-Z0-9]*$/
     const test = regex.test(name)
@@ -74,9 +69,11 @@ app.post('/api/drive/:folder/:name?', (req, res) => {
             })
     } else {
         res.sendStatus(400)
+        console.log('Le nom du dossier ne doit contenir que des caractères alphanumériques')
     }
 })
 
+//Traitement lors d'une requête de type 'DELETE' à la racine du drive et qui appelle la fonction qui supprime un dossier ou un fichier
 app.delete('/api/drive/:name?', (req, res) => {
     const name = req.params.name
 
@@ -89,9 +86,10 @@ app.delete('/api/drive/:name?', (req, res) => {
         })
 })
 
-app.delete('/api/drive/:folder/:name?', (req, res) => {
+//Traitement lors d'une requête de type 'DELETE' à l'emplacement où l'on se trouve et qui appelle la fonction qui supprime un dossier ou un fichier
+app.delete('/api/drive/*/:name', (req, res) => {
     const name = req.params.name;
-    const folder = req.params.folder;
+    const folder = req.params[0];
 
     drive.deleteFolderOrFile(name, folder).then(result => {
         res.send(result)
@@ -101,7 +99,7 @@ app.delete('/api/drive/:folder/:name?', (req, res) => {
         })
 })
 
-
+//Traitement lors d'une requête de type 'PUT' à la racine du drive et qui appelle la fonction qui permet d'uploader un fichier
 app.put('/api/drive', (req, res) => {
     const file = req.files.file.file
     const name = req.files.file.filename
@@ -115,10 +113,12 @@ app.put('/api/drive', (req, res) => {
 
 })
 
-app.put('/api/drive/:folder', (req, res) => {
+//Traitement lors d'une requête de type 'PUT' à l'emplacement où l'on se trouve et qui appelle la fonction qui permet d'uploader un fichier
+app.put('/api/drive/*', (req, res) => {
     const file = req.files.file.file
-    const folder = req.params.folder
+    const folder = req.params[0]
     const name = req.files.file.filename
+
 
     drive.uploadFile(file, name, folder).then(result => {
         res.send(result)
@@ -127,7 +127,6 @@ app.put('/api/drive/:folder', (req, res) => {
             console.log(err)
         })
 })
-
 
 module.exports = {
     start: start,
